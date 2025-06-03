9e8d5bdfe5bbcb5f53b7b692e138a987f03cb943
Lya Mun <lyamun2@illinois.edu>
2025-06-04 03:25:05 +0900

    Initial Commit

---
+++ subleaseFinder/subleaseFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  subleaseFinderApp.swift
+//  subleaseFinder
+//
+//  Created by Lya Mun on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct subleaseFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
